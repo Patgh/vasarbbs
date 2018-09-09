ae9cd12ef3c2d710e2c4f2869101188405492073
vasar <wenming.pan@foxmail.com>
2018-09-09 16:10:26 +0000

    user can create replu

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,9 +9,19 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+//    public function saved(Reply $reply)
+//    {
+//        $reply->topic->increment('reply_count', 1);
+//    }
+
+    public function created(Reply $reply)
+    {
+        $reply->topic->increment('reply_count');
+    }
+
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)

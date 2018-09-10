151f866f56a74c3bd349af69b7e569eba0d5acb4
vagrant <vagrant@homestead>
2018-09-10 17:05:30 +0000

    sidebar active users

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Handlers\ImageUploadHandler;
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
@@ -16,14 +17,17 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic, User $user)
 	{
 		//$topics = Topic::paginate(30);
 
 		$topics = $topic->withOrder($request->order)->paginate(20);
 		// 延迟预加载
 		//$topics->load(['user', 'category']);
-		return view('topics.index', compact('topics'));
+
+        $active_users = $user->getActiveUsers();
+        //dd($active_users);
+		return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Topic $topic, Request $request)
